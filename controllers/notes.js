1709b9e14094c1694921f1af99cb26f75fde7bdf
unknown <dipjal5678@gmai.com>
2023-09-05 19:47:21 +0545

    exercise before 4.8

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,10 +1,9 @@
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
 
-notesRouter.get("/", (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes);
-  });
+notesRouter.get("/", async (request, response) => {
+  const notes = await Note.find({});
+  response.json(notes);
 });
 
 notesRouter.get("/:id", (request, response, next) => {
@@ -19,7 +18,7 @@ notesRouter.get("/:id", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-notesRouter.post("/", (request, response, next) => {
+notesRouter.post("/", async (request, response) => {
   const body = request.body;
 
   const note = new Note({
@@ -28,23 +27,17 @@ notesRouter.post("/", (request, response, next) => {
     date: new Date(),
   });
 
-  note
-    .save()
-    .then((savedNote) => {
-      response.json(savedNote);
-    })
-    .catch((error) => next(error));
+  const savedNote = await note.save();
+  response.status(201).json(savedNote);
 });
 
-notesRouter.delete("/:id", (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
+notesRouter.delete("/:id", (request, response) => {
+  Note.findByIdAndRemove(request.params.id).then(() => {
+    response.status(204).end();
+  });
 });
 
-notesRouter.put("/:id", (request, response, next) => {
+notesRouter.put("/:id", (request, response) => {
   const body = request.body;
 
   const note = {
@@ -52,11 +45,11 @@ notesRouter.put("/:id", (request, response, next) => {
     important: body.important,
   };
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then((updatedNote) => {
+  Note.findByIdAndUpdate(request.params.id, note, { new: true }).then(
+    (updatedNote) => {
       response.json(updatedNote);
-    })
-    .catch((error) => next(error));
+    }
+  );
 });
 
 module.exports = notesRouter;
