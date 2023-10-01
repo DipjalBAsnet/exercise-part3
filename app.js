1c705523b8b6dda41ee60a37da1ac98f04ab4d80
unknown <dipjal5678@gmai.com>
2023-10-01 20:10:05 +0545

    little change in  part5

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ const usersRouter = require("./controllers/users");
 const bodyparser = require("body-parser");
 const loginRouter = require("./controllers/login");
 
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
+
 mongoose.set("strictQuery", false);
 
 logger.info("connecting to", config.MONGODB_URI);
@@ -32,6 +37,7 @@ app.use(middleware.requestLogger);
 app.use("/api/notes", notesRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
